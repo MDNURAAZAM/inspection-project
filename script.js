941b016c073421c95c381cfa12ef866e08d635d2
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2022-03-19 19:21:05 +0100

    remove button completed

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 document.getElementById('add-condition').addEventListener('click', () => {
     const inputID = document.getElementById("ID");
     const idValue = parseInt(inputID.value);
+    // empty input handled
     if (!isNaN(idValue) && document.getElementById("date").value.length > 0) {
         const container = document.getElementById("inspection-details-data");
         const div = document.createElement('div');
         div.innerHTML = `
-        <div>
             <label for="station">Station: </label>
             <input type="number" id="station" name="station" required>
             <br>
             <label for="main">Main:</label>
             <input type="text" id="main" name="main" required>
             <br>
-        </div>
+            <button onclick="removeContent()">Remove</button>
+            <button onclick="updateContent()">Update</button>
+        
         `
         container.append(div);
         
@@ -22,5 +24,11 @@ document.getElementById('add-condition').addEventListener('click', () => {
         document.getElementById("date").value = "";
     }
 })
+
+//removing content on button click
+const removeContent = () => { 
+    const parentContent = event.target.parentNode;
+    parentContent.style.display = "none";
+}
 
 
